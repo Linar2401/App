e09e213e27ac0bbc141337d3d7f8299320c307e4
Zagir Dingizbaev <zagir.dingizbaev@yandex.ru>
2018-12-27 11:32:29 +0300

    Find the way command modified

--- src/commands/FindTheWayCommand.java
+++ src/commands/FindTheWayCommand.java
@@ -3,16 +3,18 @@ package commands;
 import main.App;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FindTheWayCommand extends Command {
     public void execute(App app) {
         app = app;
-        //Это место работы Загира.Все должно записаться вместе с лабиринтом в char[][] cellsWithPathpublic class Li {
-        int[][] cellsWithPath = app.getCells();
+        int[][] cells = app.getCells();
+        int[][] cellsWithPath;
+        cellsWithPath = Arrays.copyOf(cells, cells.length);
         boolean stop;
         int dx[] = {1, 0, -1, 0};
         int dy[] = {0, 1, 0, -1};
-        int d = 0;
+        int d;
         int tmpEn = 0;
         int tmpEx = 0;
         for (int i = 0; i < app.getSize(); ++i) {
@@ -26,13 +28,14 @@ public class FindTheWayCommand extends Command {
             }
         }
         cellsWithPath[tmpEn][0] = 0;
+        d = 0;
         do {
             stop = true;
-            for (int y = 0; y < app.getSize(); y++)
-                for (int x = 0; x < app.getSize(); ++x)
+            for (int y = 0; y < app.getSize() * 2 + 1; ++y)
+                for (int x = 0; x < app.getSize() * 2 + 1; ++x)
                     if (cellsWithPath[y][x] == d) {
-                        for (int k = 0; k < 4; ++k) {
-                            if (x + dx[k]>=0 && y + dy[k]>=0) {
+                        for (int k = 0; k < 4; k++) {
+                            if (x + dx[k] >= 0 && y + dy[k] >= 0) {
                                 if (cellsWithPath[y + dy[k]][x + dx[k]] == -2) {
                                     stop = false;
                                     cellsWithPath[y + dy[k]][x + dx[k]] = d + 1;
@@ -41,7 +44,7 @@ public class FindTheWayCommand extends Command {
                         }
                     }
             d++;
-        } while (!stop && cellsWithPath[tmpEx][app.getSize()] == -2);
+        } while (!stop && cellsWithPath[tmpEx][app.getSize() * 2] == -2);
         app.changeCellsWithPath(cellsWithPath);
     }
 }
